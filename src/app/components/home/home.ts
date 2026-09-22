e934482603cd19296f4dae9662486f1b97531302
Emanuel Newtech <vitorfons@hotmail.com>
2026-09-22 03:04:30 -0300

    feat: Atualização home, Inserido estado em componentes, registro de atributos dinâmicos com validação via botões e console, Injeção de métodos das classes em a eventos do html + validação estrutura condicional Angular.

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -6,4 +6,24 @@ import { Component } from '@angular/core';
   styleUrl: './home.css',
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+  tstbooleano = false;
+  num = "2026";
+  idButton = "ntw";
+  mostrarteste = false;
+
+  atualizarTstBooleano(valor: boolean) 
+  {
+    this.tstbooleano = valor;
+  }
+
+
+
+  submit() {
+    console.log(" Teste realizado com sucesso! ");
+  } 
+
+  submiteve(event: any) {
+    console.log(" Teste realizado com sucesso! ", event);
+  }
+}
