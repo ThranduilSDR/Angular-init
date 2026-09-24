0ba7683f3652db5f15102aaadd57a171ada470c5
Emanuel Newtech <vitorfons@hotmail.com>
2026-09-24 16:33:54 -0300

    feat: Adicionado testes de estrutura de repetição e condicionais como array exibindo em tela as listas.

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -13,6 +13,9 @@ export class Home {
   mostrarteste = false;
   autor = "ThranduilSDR";
   finalidade = "Validação Conceitos Iniciais Angular ";
+  listItens = ["monitor", "teclado", "mouse", "gabinete", "fonte", "placa de vídeo", "placa mãe", "processador", "memória ram", "ssd"];
+  listitensid = [{id: 1, nome: "monitor"}, {id: 2, nome: "teclado"}, {id: 3, nome: "mouse"}, {id: 4, nome: "gabinete"}, {id: 5, nome: "fonte"}, {id: 6, nome: "placa de vídeo"}, {id: 7, nome: "placa mãe"}, {id: 8, nome: "processador"}, {id: 9, nome: "memória ram"}, {id: 10, nome: "ssd"}];
+
 
   atualizarTstBooleano(valor: boolean) 
   {
